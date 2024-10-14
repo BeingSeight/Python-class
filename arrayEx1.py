8fae3a3d06afa3ae8c4328baebf09828f10e8447
BeingSeight <ss9656484@gmail.com>
2024-10-14 11:55:43 +0530

    initial commit

---
+++ arrayEx1.py
@@ -0,0 +1,12 @@
+import array
+
+a = array.array('i', [1, 2, 3])
+'''
+print("the new created array is : ", end=" ")
+print(a[0])
+print(a[3])
+
+b = array.array('d', [2.5, 3.2, 3.3])
+print("the new created array is : ", end=" ")
+print(b[0])
+print(b[3])
